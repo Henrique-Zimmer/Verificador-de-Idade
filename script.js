8c4d62504e8d8f332fb852167f12316a6aca7123
Henrique Zimmer <henriquezimmerwarren06@gmail.com>
2025-04-01 14:40:16 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,60 @@
+/*function verificar(){
+    var ano = document.getElementById('ano')
+    var homem = document.getElementById('masculino')
+    var mulher = document.getElementById('mulher')
+    var idade = 2025 - ano
+    if (ano >= 1910 && ano <1965){
+        res.innerHTML = `Detectamos uma pessoa com ${idade} anos.`
+    }
+
+}*/
+
+function verificar(){
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('ano')
+    var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || Number(fano.value) > ano ) {
+        alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+       var fsex = document.getElementsByName('sexo')
+       var idade = ano - Number(fano.value)
+       var genero = ''
+       var img = document.createElement('img')
+       img.setAttribute('id', 'foto')
+       if (fsex[0].checked){
+        genero = 'Homem'
+        if (idade >= 0 && idade < 10){
+            //Criança
+            img.setAttribute('src', 'imagens/bebe-menino.png')
+        } else if (idade < 21){
+            //Jovem
+            img.setAttribute('src', 'imagens/jovem-menino.png')
+        } else if (idade < 50){
+            //adulto
+            img.setAttribute('src', 'imagens/adulto.png')
+        } else {
+            //idoso
+            img.setAttribute('src', 'imagens/idoso.png')
+        }
+       } else if (fsex[1].checked){
+        genero = 'Mulher'
+        if (idade >= 0 && idade < 10){
+            //Criança
+            img.setAttribute('src', 'imagens/bebe-menina.png')
+        } else if (idade < 21){
+            //Jovem
+            img.setAttribute('src', 'imagens/jovem-menina.png')
+        } else if (idade < 50){
+            //adulto
+            img.setAttribute('src', 'imagens/adulta.png')
+        } else {
+            //idoso
+            img.setAttribute('src', 'imagens/idosa.png')
+        }
+       }
+       res.innerHTML =`Detectamos ${genero} com ${idade} anos.`
+       res.style.textAlign = 'Center'
+       res.appendChild(img)
+    }
+}
